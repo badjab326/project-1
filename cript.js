272ef7290a82d6118b9459396c6851774719a2e2
“julian <badjab326@yahoo.com>
2022-05-09 06:30:59 -0700

    added some styling, element organization

---
+++ cript.js
@@ -0,0 +1,6 @@
+//Variables
+const URL = "https://rawg.io/api/games?key=65a0d6caa83f42fdaace4080c7d3d576&search=${game}"
+
+//Element References
+const form$ = $('form');
+const input$ = $(`input[type="text"]`);
