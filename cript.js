3492ab089d4ee43983a648768b0d4413c1135ae1
“julian <badjab326@yahoo.com>
2022-05-10 06:50:26 -0700

    created variable to return random results

--- cript.js
+++ cript.js
@@ -18,14 +18,17 @@ function loadData(evt){
     evt.preventDefault()
     userInput = input$.val()
     const URL = `https://api.rawg.io/api/games?page_size=50&genres=${userInput}&key=65a0d6caa83f42fdaace4080c7d3d576`
+    
+
 
     $.ajax(URL).then(function(data) {
         // console.log(data)
         // title$.text('')
         // result$.text('')
-        title$.text(data.results[0].name)
-        result$.html(`<img src="${data.results[0].background_image}"/>`)
-        const plats = data.results[0].platforms.map(plat => {
+        const x = Math.floor((Math.random() * data.results.length) + 0)
+        title$.text(data.results[x].name)
+        result$.html(`<img src="${data.results[x].background_image}"/>`)
+        const plats = data.results[x].platforms.map(plat => {
             console.log(plat.platform.name)
             return `${plat.platform.name} `
         })
